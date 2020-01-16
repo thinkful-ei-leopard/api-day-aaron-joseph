1fb7779f4d572a3a2eb1de95ad56577143a215f3
Neruul <viralze@live.com>
2020-01-16 09:49:12 -0800

    pushing before lunch

---
+++ src/api.js
@@ -0,0 +1,35 @@
+export default { 
+  getItems,
+  createItem
+      
+};
+
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/aaron-joseph';
+
+function getItems(){
+  return fetch(`${BASE_URL}/items`);
+}
+const createItem = function name(){
+  let newItem = JSON.stringify(name);
+  return fetch('${BASE_URL}/items'),{
+    method: 'POST',
+    headers: {' Content-Type': 'application/json' },
+    body: newItem
+  };
+};
+
+
+// const blogData = JSON.stringify({
+//     title: "Because I'm Batman",
+//     author: 'Bruce Wayne',
+//     content: 'No one will ever guess my secret identity. Haha!'  
+//   });
+  
+//   fetch('http://example.api.com/blogs', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: blogData
+//   })
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.error(err.message));
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import $ from 'jquery';
-
+import api from './api';
 import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
 
 const main = function () {
-  shoppingList.bindEventListeners();
-  shoppingList.render();
+
 };
 
 $(main);
