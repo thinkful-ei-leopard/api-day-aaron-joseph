0ec6c64bbad03b93242fb2830ccb83c62f690331
Aaron Turcios <aar.turcios@gmail.com>
2020-01-16 18:03:46 -0800

    step 9

--- src/index.js
+++ src/index.js
@@ -5,19 +5,13 @@ import './index.css';
 import store from './store';
 import shoppingList from './shopping-list';
 
-const main = function () {
-  api.getItems()
-    .then(res => res.json())
-    .then((items) => {
-      items.forEach((item) => store.addItem(item));
-      shoppingList.render();
-      const item = store.items[0];
-      console.log('current name: ' + item.name);
-      store.findAndUpdate(item.id, { name: 'barbaz' });
-      console.log('new name: ' + item.name);
-    });
-  shoppingList.bindEventListeners();
-  shoppingList.render();
+
+  const main = function () {
+    api.getItems()
+      .then(res => res.json())
+      .then((items) => {
+        items.forEach((item) => store.addItem(item));
+        shoppingList.render();
   
 };
 
